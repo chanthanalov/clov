31a26459fbd5efdb8b1874466d5f890910ba2d41
chanthana <158802730+chanthanalov@users.noreply.github.com>
2026-02-13 22:49:39 +1000

    pages upload

---
+++ java.js
@@ -0,0 +1,13 @@
+var i = 0
+var txt = 'Chanthana Lov'   /* The text */
+var speed = 100 /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+    if (i < txt.length) {
+        document.getElementById("typewriter").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+}
+
+typeWriter();
